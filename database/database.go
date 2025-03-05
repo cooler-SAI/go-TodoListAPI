a1fdbbc65d30d084a59d4837bf8ff29d059e6056
cooler-SAI <cooler19860212@yahoo.com>
2025-03-05 22:57:59 +0300

    DB connection added

--- database/database.go
+++ database/database.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"go-TodoListAPI/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() error {
+	database, err := gorm.Open(sqlite.Open("todos.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+		return err
+	}
+
+	DB = database
+
+	err = DB.AutoMigrate(&models.User{}, &models.Todo{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- models/todo.go
+++ models/todo.go
@@ -1,1 +1,8 @@
 package models
+
+type Todo struct {
+	ID          uint   `gorm:"primaryKey"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	UserID      uint   `json:"user_id"`
+}
