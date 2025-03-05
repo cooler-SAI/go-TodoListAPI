1a1e6b8ef75056f8b577ab95f42eb9d0d1dda72c
cooler-SAI <cooler19860212@yahoo.com>
2025-03-05 23:11:19 +0300

    DB connection added, base code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"go-TodoListAPI/database"
+	"log"
+)
+
 func main() {
 
+	err := database.ConnectDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	fmt.Println("Connected to database")
 }
