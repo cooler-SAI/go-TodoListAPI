aaa546cfddf099ad519e91ee2e9e96ea14ac36b2
cooler-SAI <cooler19860212@yahoo.com>
2025-03-06 19:58:25 +0300

    set up auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,1 +1,75 @@
 package auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"go-TodoListAPI/models"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"os"
+	"time"
+)
+
+var jwtKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+type Claims struct {
+	UserID uint `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func GenerateJWT(user models.User) (string, error) {
+	expirationTime := time.Now().Add(1 * time.Hour)
+	claims := &Claims{
+		UserID: user.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	return tokenString, err
+}
+
+func ValidateJWT(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if !token.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+	return claims, nil
+}
+
+func RegisterUser(db *gorm.DB, user *models.User) error {
+	hashedPassword, err := HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = hashedPassword
+	return db.Create(user).Error
+}
+
+func LoginUser(db *gorm.DB, email, password string) (string, error) {
+	var user models.User
+	if err := db.Where("email = ?", email).First(&user).Error; err != nil {
+		return "", err
+	}
+	if !CheckPasswordHash(password, user.Password) {
+		return "", jwt.ErrSignatureInvalid // Неверный пароль
+	}
+	token, err := GenerateJWT(user)
+	return token, err
+}
